8928b92eedd6bf4b8b2cc2a5109a80ebc1e60ad1
UARKHAWG <uarkhawg@gmail.com>
2024-05-10 22:46:31 -0500

    linked list attribute addition

--- linked-lists.py
+++ linked-lists.py
@@ -14,4 +14,13 @@ class LinkedList:
     def __init__(self) -> None:
         self.head = None
         self.tail = None
-        
+
+    def append(self, data):
+        new_node = ListNode(data)
+        if not self.head:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+            
